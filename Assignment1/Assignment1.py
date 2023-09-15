23cdfa5056c4b9dcf508ee26fa7ed20e16b99d2d
Nicholas Tussing <tussingn24@mail.wlu.edu>
2023-09-15 17:40:16 -0400

    Adding tests

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -6,33 +6,35 @@ map based on a list of numbers. This solution utilizes memoization as an
 implementation of dynamic programming for an efficient solution
 """
 
-def trap(heights):
+def trap(map):
     trapped_water = 0
     index = -1
-    length = len(heights)
+    length = len(map)
+    #Creating arrays for the mamximum heights to the left and right position 
     leftMax = [0] * length  
     rightMax = [0] * length  
     
+    #finding the left maximum 
+    leftMax[0] = map[0]
+    for i in range(1,length):
+        leftMax[i] = max(leftMax[i-1],map[i])
     
-    for i in range(0,length):
-        index +=1
-        if i == 0 and heights[i] == 0:
-            continue
-        if index == length-1:
-            return trapped_water
-        if heights[i] >=1:
-            for n in range(i+1,length):
-                if heights[n]< heights[i]:
-                    trapped_water += heights[i]-heights[n]
-                elif heights[n]> heights[i]: 
-                    break
+    #finding the right maximum, iterating from the right side of the array
+    rightMax [length-1] = map[length -1]
+    for i in range(length -2, -1, -1):
+        rightMax[i] = max(rightMax[i+1], map[i])
+        
+    #Calculating the water trapped based on the max heights for each position 
+    for i in range(length):
+        minMax = min(leftMax[i], rightMax[i])
+        trapped_water += max(minMax - map[i], 0)
     return trapped_water
 
 
 # Testing with example elevation map
 if __name__ == "__main__":
-    elevationMapTest = [0,1,0,2,1,0,1,3,2,1,2,1]
-    print(trap(elevationMapTest))
+    print("Testing the function")
+    print(trap("Expected: 6 \n Result: " [0,1,0,2,1,0,1,3,2,1,2,1],))
     print(trap([0,1,0,2,0,1,0,2,0,2]))
     
 
