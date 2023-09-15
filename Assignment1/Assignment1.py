86c2edc4d04aee9ef07b0a055dc961fef485ec9e
Nicholas Tussing <tussingn24@mail.wlu.edu>
2023-09-15 16:54:45 -0400

    committing latest save

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -14,27 +14,24 @@ def trap(heights):
         index +=1
         if i == 0 and heights[i] == 0:
             continue
-        if heights[i] >=1 and index < length:
-            for n in range(i,length):
-                if heights[n]<= heights[i]:
+        if index == length-1:
+            return trapped_water
+        if heights[i] >=1:
+            for n in range(i+1,length):
+                if heights[n]< heights[i]:
                     trapped_water += heights[i]-heights[n]
                 elif heights[n]> heights[i]: 
-                    continue
+                    break
     return trapped_water
-            
-
 
 
 # Testing with example elevation map
 if __name__ == "__main__":
     elevationMapTest = [0,1,0,2,1,0,1,3,2,1,2,1]
     print(trap(elevationMapTest))
+    print(trap([0,1,0,2,0,1,0,2,0,2]))
+    
 
-
-# Testing the ElevationMap class
-if __name__ == "__main__":
-    elevationMapTest = [0,1,0,2,1,0,1,3,2,1,2,1]
-    print(trap(elevationMapTest))
     
 
 
