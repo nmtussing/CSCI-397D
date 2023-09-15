e740cbb942f9b0ce84fbce2093c25cdb5d508ed4
Nicholas Tussing <tussingn24@mail.wlu.edu>
2023-09-14 23:00:01 -0400

    Initial loop

---
+++ Assignment1.py
@@ -0,0 +1,27 @@
+"""
+Assignment1.py
+Author: Nicholas Tussing
+A program that computes how much water would be contained in an elevation 
+map based on a list of numbers. This solution utilizes memoization as an 
+implementation of dynamic programming for an efficient solution
+"""
+
+def trap(heights):
+    length = len(heights)
+    for i in range(0,length):
+        if heights[i] == 0:
+            
+    
+    
+    def __init__(self, heights):
+        self.heights = heights
+
+    d
+
+# Testing the ElevationMap class
+if __name__ == "__main__":
+    elevation_map = ElevationMap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
+    trapped_water = elevation_map.trap_water()
+    print("Trapped Water:", trapped_water)
+
+
