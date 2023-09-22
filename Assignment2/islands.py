6e679609ad941bc0666183973e48a27528dd08d8
Nicholas Tussing <tussingn24@mail.wlu.edu>
2023-09-22 11:50:54 -0400

    Committing first initial classes

---
+++ Assignment2/islands.py
@@ -0,0 +1,17 @@
+""" 
+Author: Nicholas Tussing 
+islands.py
+Creating the island class for island objects 
+"""
+
+class islands:
+    def __init__(self):
+        self.number = 0
+        self.hasTreasure = False
+        
+        
+    def hasTreasure(self):
+        if self.hasTreasure == False:
+            return False
+        else:   
+            return True
