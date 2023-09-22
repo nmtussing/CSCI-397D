f5327a7844af631eaf5e548f96db0d395453f2d4
Nicholas Tussing <tussingn24@mail.wlu.edu>
2023-09-22 13:55:42 -0400

    Creating first go around of the probability matrix

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -5,10 +5,55 @@ Program to run the MDP with 10 states, truncated to 25 steps
 """
 import  agent
 import islands  
+# Creating the environment 
+island1 = islands.islands()
+island1.number = 1
+island2 = islands.islands()
+island2.number = 2
+island3 = islands.islands()
+island3.number = 3
+island3.treasure = True
+island4 = islands.islands()
+island4.number = 4
+island5 = islands.islands()
+island5.number = 5
+island6 = islands.islands()
+island6.number = 6
+island6.treasure = True
+island7 = islands.islands()
+island7.number = 7
+island8 = islands.islands()
+island8.number = 8
+island9 = islands.islands()
+island9.number = 9
+island9.treasure = True
+island10 = islands.islands()
+island10.number = 10
+
+actionset = ['dig','moveNext']
+#Creating instance of the probability matrix where the first column is the island number
+#and second column is the probability to dig, and the remaining columns are the probability to 
+#move to the other islands
+probabilityMatrix = [
+    [1, 0.1],
+    [2, 0.1],
+    [3, 0.1],
+    [4, 0.1],
+    [5, 0.1],
+    [6, 0.1],
+    [7, 0.1],
+    [8, 0.1],
+    [9, 0.1],
+    ]
+
+
 
 if __name__ == "__main__":
     agent1 = agent.agent()
     print(agent1.location)
     agent1.location = 10
     print(agent1.location)
+    print(island6.hasTreasure())
+    
+        
     
--- Assignment2/islands.py
+++ Assignment2/islands.py
@@ -7,19 +7,18 @@ Creating the island class for island objects
 class islands:
     def __init__(self):
         self.number = 0
-        self.hasTreasure = False
+        self.treasure = False
         self.terminal = False
         
-        
     def hasTreasure(self):
-        if self.hasTreasure == True:
+        if self.treasure == True:
             return True
         else:   
             return False
         
-    
     def isTerminal(self):
         if self.terminal == True:
             return True
         else:
             return False
+        
